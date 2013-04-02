e792947ca0caddd43ebca99c33a3ffb7289b46e7
Fabian Vogler <fabian@equivalence.ch>
2013-04-02 22:34:19 +0200

    Added Instagram bundle

---
+++ src/FriendScore/InstagramBundle/Controller/DefaultController.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace FriendScore\InstagramBundle\Controller;
+
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use Guzzle\Http\Client;
+
+class DefaultController extends Controller
+{
+    protected $clientId = '9f0cca2544ce4f43adfa4122ee3cfab4';
+    protected $clientSecret = 'a4ade51db675461b81674313c789c8dc';
+
+    protected $redirectUri = 'http://localhost:8000/instagram/callback';
+
+    protected $client;
+
+    protected $code = '';
+    protected $accessToken = '';
+
+    public function __construct()
+    {
+        $this->client = new Client('https://api.instagram.com');
+    }
+
+    /**
+     * @Route("/")
+     * @Template()
+     */
+    public function indexAction()
+    {
+        $accessToken = '1982948.9f0cca2.3db8865868274573b68a41e052a0071b';
+
+        if ($accessToken) {
+            
+            // API call
+            $request = $this->client->get('v1/users/self/feed');
+            $request->getQuery()->set('access_token', $accessToken);
+            $response = $request->send();
+
+            $body = $response->getBody();
+            var_dump(json_decode($body));
+        }
+
+        return array('client_id' => $this->clientId, 'redirect_uri' => $this->redirectUri);
+    }
+
+    /**
+     * @Route("/callback")
+     */
+    public function callbackAction(Request $request)
+    {
+        $code = $request->get('code');
+
+        $request = $this->client->post('oauth/access_token', null, array (
+            'client_id' => $this->clientId,
+            'client_secret' => $this->clientSecret,
+            'grant_type' => 'authorization_code',
+            'redirect_uri' => $this->redirectUri,
+            'code' => $code,
+        ));
+        $response = $request->send();
+
+        $body = $response->getBody();
+
+        return new Response($body);
+    }
+}
---
+++ src/FriendScore/InstagramBundle/FriendScoreInstagramBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace FriendScore\InstagramBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class FriendScoreInstagramBundle extends Bundle
+{
+}
