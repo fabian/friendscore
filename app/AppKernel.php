dd3793add4e01ada63d27818253cf2bf0219919a
gabac <gabathuler@gmail.com>
2013-06-05 00:28:18 +0200

    Added twitter

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -20,6 +20,7 @@ class AppKernel extends Kernel
             new JMS\DiExtraBundle\JMSDiExtraBundle($this),
             new JMS\SecurityExtraBundle\JMSSecurityExtraBundle(),
             new FriendScore\FacebookBundle\FriendScoreFacebookBundle(),
+            new FriendScore\TwitterBundle\FriendScoreTwitterBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
