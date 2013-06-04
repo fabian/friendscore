c2fa402bcb789d0a87b0a7a95f5405c2fc2b055a
Fabian Vogler <fabian@equivalence.ch>
2013-06-05 00:03:38 +0200

    Added Pagoda Box support

---
+++ app/config/parameters.php
@@ -0,0 +1,19 @@
+<?php
+
+$container->setParameter('database_driver', 'pdo_mysql');
+$container->setParameter('database_host', 'localhost');
+$container->setParameter('database_port', null);
+$container->setParameter('database_name', 'friendscore');
+$container->setParameter('database_name_test', 'friendscore_test');
+$container->setParameter('database_user', 'root');
+$container->setParameter('database_password', null);
+$container->setParameter('database_path', null);
+
+$container->setParameter('mailer_transport', 'smtp');
+$container->setParameter('mailer_host', 'localhost');
+$container->setParameter('mailer_user', null);
+$container->setParameter('mailer_password', null);
+$container->setParameter('locale', 'en');
+$container->setParameter('secret', 'd374ac3307aa6c27dfbec09d196945c4');
+
+$container->setParameter('elasticsearch_endpoint', 'http://nlypceit.api.qbox.io');
