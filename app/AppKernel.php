6cd726b5efd4960ba132a632db82b801e2599e34
Fabian Vogler <fabian@equivalence.ch>
2013-06-04 22:41:42 +0200

    Added user bundles

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -19,6 +19,8 @@ class AppKernel extends Kernel
             new JMS\AopBundle\JMSAopBundle(),
             new JMS\DiExtraBundle\JMSDiExtraBundle($this),
             new JMS\SecurityExtraBundle\JMSSecurityExtraBundle(),
+            new FOS\UserBundle\FOSUserBundle(),
+            new FriendScore\UserBundle\FriendScoreUserBundle(),
             new FriendScore\FoursquareBundle\FriendScoreFoursquareBundle(),
         );
 
---
+++ src/FriendScore/UserBundle/FriendScoreUserBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace FriendScore\UserBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class FriendScoreUserBundle extends Bundle
+{
+}
