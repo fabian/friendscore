c7a3d2485ce7018a4632e007c786f4ae4cdbae80
gabac <gabathuler@gmail.com>
2013-06-16 00:16:55 -0700

    rough version of friend score

--- src/FriendScore/WebBundle/Controller/DefaultController.php
+++ src/FriendScore/WebBundle/Controller/DefaultController.php
@@ -150,7 +150,109 @@ class DefaultController
                 $visitors[] = $result->getData();
             }
         }
-
+        
+        //$place->friendscore = '0.83';
+        $place->friendscore = $this->calcualteFriendScore($user, $place->name);
+        
         return array('place' => $place, 'visitors' => $visitors);
+    }
+    
+    protected function calcualteFriendScore($user, $placeName) {
+        $index = $this->elastica->getIndex('friendscore');
+        
+        $userId = $user->getId();
+        
+        $userQuery = new \Elastica\Query\Term(array('user_id' => $userId));
+        $hasChildQuery = new \Elastica\Query\HasChild($userQuery, 'visit');
+        $stringQuery = new \Elastica\Query\QueryString('*' . $placeName . '*');
+
+        $boolQuery = new \Elastica\Query\Bool();
+        $boolQuery->addMust($hasChildQuery);
+        $boolQuery->addMust($stringQuery);
+
+        $query = new \Elastica\Query();
+        $query->setQuery($boolQuery);
+        $query->setSize(5);
+        $resultSet = $index->getType('place')->search($query);
+
+        $places = array();
+        
+        $friendsCheckins = array();
+        
+        foreach ($resultSet->getResults() as $result) {
+            $placeId = $result->getData()['id'] ;
+            
+            $place = $index->getType('place')->getDocument($placeId);
+
+            $visitors = array();
+ 
+            $userForVisitQuery = new \Elastica\Query\Term(array('user_id' => $userId));
+            $placeForVisitQuery = new \Elastica\Query\Term(array('place_id' => $placeId));
+
+            $boolForVisitQuery = new \Elastica\Query\Bool();
+            $boolForVisitQuery->addMust($userForVisitQuery);
+            $boolForVisitQuery->addMust($placeForVisitQuery);
+
+            $visitQuery = new \Elastica\Query();
+            $visitQuery->setQuery($boolForVisitQuery);
+            $visitQuery->setSort(array('last_checkin' => 'desc'));
+            $visitQuery->setSize(5);
+            $visitResultSet = $index->getType('visit')->search($visitQuery);
+
+            foreach ($visitResultSet->getResults() as $visitResult) {
+
+                //change to id
+                $friendId = $visitResult->getData()['last_name'];
+                
+                //check start of string for fb or fq
+                $checkinsToAdd = null;
+                
+                if(true) {
+                    if(!isset($friendsCheckins['facebook'])) {
+                        $friendsCheckins['facebook'] = array();
+                    }
+                    
+                    $service = 'facebook';
+                } else {
+                    if(!isset($friendsCheckins['foursquare'])) {
+                        $friendsCheckins['foursquare'] = array();
+                    }  
+
+                    $service = 'foursquare';
+                }
+                
+                if(isset($friendsCheckins[$service][$friendId])) {
+                    $friendsCheckins[$service][$friendId]++;
+                } else {
+                    $friendsCheckins[$service][$friendId] = 1;
+                }
+            }
+        }
+        
+        //how to do a constant?
+        $baspoint = 1 / (3 * 2) / 2;
+        
+        $friendscore = 0;
+
+        foreach($friendsCheckins as $service) {
+            $serviceScore = 0;
+
+            foreach($service as $friendCheckin) {
+                if($friendCheckin > 1) {
+                    $serviceScore += $baspoint * 2;
+                } else {
+                    $serviceScore += $baspoint;
+                }
+            }
+   
+            if($serviceScore > 0.5) {
+                $serviceScore = 0.5;
+            }
+            
+            $friendscore += $serviceScore;
+            $serviceScore = 0;
+        }
+        
+        return $friendscore;
     }
 }
